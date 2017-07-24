ea2c4ff25e6f47266e651c779afacbe7b49bf3d0
kh498 <klink100@gmail.com>
2017-07-24 18:53:15 +0200

    Enable unlimited FPS

--- desktop/src/no/kh498/motc/desktop/DesktopLauncher.java
+++ desktop/src/no/kh498/motc/desktop/DesktopLauncher.java
@@ -2,11 +2,14 @@ package no.kh498.motc.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import no.kh498.motc.Motc;
+import no.kh498.motc.MotC;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Motc(), config);
-	}
+    public static void main(final String[] arg) {
+        final LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.vSyncEnabled = false;
+//        config.fullscreen = true;
+        config.foregroundFPS = -1;
+        new LwjglApplication(new MotC(), config);
+    }
 }
